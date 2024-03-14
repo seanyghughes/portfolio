a0fc77af8c1f4d1582ddbb77b3a6c386e627176a
Sean Hughes <76781386+seanyghughes@users.noreply.github.com>
2024-03-14 13:23:49 +0000

    Setup deployment pipeline

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,14 @@
 import ProfilePic from './Profile-Pic.jpg';
 import './App.css';
+import Header from './components/Header.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className='NavBar'>
+      <Header /> {/* Include the Header component */}
+      </div>
         <p>
           Site Currently Under Redevelopment
         </p>
