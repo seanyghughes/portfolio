f171991d1f3fb89dcecccc5c86f3592b758f7c73
Sean Hughes <76781386+seanyghughes@users.noreply.github.com>
2024-04-04 13:28:16 +0100

    Restart project

---
+++ seangerardhughes/src/App.js
@@ -0,0 +1,19 @@
+import ProfilePic from './ProfilePic.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Site Currently Under Redevelopment</h2>
+        <img src={ProfilePic} className="ProfilePic" alt="ProfilePic" />
+        <p>
+          Sean Gerard Hughes | Analytics 
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
